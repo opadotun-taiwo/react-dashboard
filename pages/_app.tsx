ff55a49343dfe84b03f6fc0b6d8e2875bf0962e7
OPADOTUN TAIWO <opadotunoluwaseun@yahoo.com>
2023-07-26 06:36:30 +0100

    fixed build error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import Sidebar from '../components/SideBar'
+import Sidebar from '../components/Sidebar'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
